fc4f5b3870dcf9637a4f175bb218ccbeeea7e710
ryhoneyman <ryhoneyman@gmail.com>
2024-02-25 13:28:21 +0000

    updated deferred logging output

--- lib/debug.class.php
+++ lib/debug.class.php
@@ -47,17 +47,24 @@ include_once 'base.class.php';
 class Debug extends Base
 {
    protected $version = 1.0;
+   protected $level   = 0;
+   protected $type    = null;
+   protected $buffer  = false;
+   public    $log     = array();
 
    //===================================================================================================
    // Description: Creates the class object
    // Input: int(level), Default level for debug filtering
    // Input: int(type), Display debug type: DEBUG_HTML/COMMENT/CLI
+   // Input: array(options), Class control options
    // Output: null()
    //===================================================================================================
-   public function __construct($level = 0,$type = DEBUG_COMMENT)
+   public function __construct($level = 0, $type = DEBUG_COMMENT, $options = null)
    {
       $this->type($type);
       $this->level($level);
+
+      if (isset($options['buffer'])) { $this->buffer($options['buffer']); } 
    }
 
    //===================================================================================================
@@ -67,7 +74,7 @@ class Debug extends Base
    // Input: int(caller), Caller index in the backtrace array
    // Output: null()
    //===================================================================================================
-   public function trace($level,$mesg,$caller = 1)
+   public function trace($level, $mesg, $caller = 1)
    {
       $current = $this->level();
 
@@ -84,9 +91,23 @@ class Debug extends Base
       $file  = preg_replace('/^.*\//','',$trace[$caller]['file']);
       $class = ($trace[$caller]['class']) ? $trace[$caller]['class'] : "Main";
 
-      printf("%s[%s] %s (%s, %s, %s:%s) %s%s\n",
-             ($this->comment()) ? "<!-- " : "",$date,$level,$class,$func,$file,$line,$mesg,
-             ($this->comment()) ? " -->" : (($this->htmlize()) ? "<br>" : ""));
+      $output = sprintf("%s[%s] %s (%s, %s, %s:%s) %s%s\n",
+                        ($this->comment()) ? "<!-- " : "",$date,$level,$class,$func,$file,$line,$mesg,
+                        ($this->comment()) ? " -->" : (($this->htmlize()) ? "<br>" : ""));
+
+      if ($this->buffer()) { $this->log[] = $output; }
+      else { print $output; }
+   }
+
+   //===================================================================================================
+   // Description: Gets (and sets) current debug buffer mode
+   // Input: bool(value), Requested buffer mode, if supplied
+   // Output: bool(store), Current buffer setting
+   //===================================================================================================
+   public function buffer($value = null)
+   {
+      if (isset($value)) { $this->buffer = ($value) ? true : false; }
+      return (($this->buffer) ? true : false);
    }
 
    //===================================================================================================
@@ -96,8 +117,8 @@ class Debug extends Base
    //===================================================================================================
    public function level($level = null)
    {
-      if (isset($level)) { $this->self['level'] = $level; }
-      return $this->self['level'];
+      if (isset($level)) { $this->level = $level; }
+      return $this->level;
    }
 
    //===================================================================================================
@@ -107,8 +128,8 @@ class Debug extends Base
    //===================================================================================================
    public function type($type = null)
    {
-      if (isset($type)) { $this->self['type'] = $type; }
-      return $this->self['type'];
+      if (isset($type)) { $this->type = $type; }
+      return $this->type;
    }
 
    //===================================================================================================
@@ -125,6 +146,25 @@ class Debug extends Base
    // Output: boolean(value), True if COMMENT debugging is enabled
    //===================================================================================================
    public function comment() { return ($this->type() == DEBUG_COMMENT); }
+
+   //===================================================================================================
+   // Description: Returns current buffered log, then flushes it
+   // Output: string(log), Current log buffer
+   //===================================================================================================
+   public function getLog()
+   {
+      $data = $this->log;
+      $this->log = array();
+      return $data;
+   }
+
+   public function writeFile($fileName, $mesg, $append = null)
+   {
+      $flags = (is_null($append) || $append === true) ? FILE_APPEND : 0;
+
+      return @file_put_contents($fileName,sprintf("[%s] %s\n",date('Y-m-d H:i:s'),$mesg),$flags);
+   }
+
 }
 
 ?>
